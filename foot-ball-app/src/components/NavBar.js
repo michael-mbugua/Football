b11f4da1697101e37d1ef299b1229114a6dc72d0
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-10-10 13:34:53 +0300

    create naigation component

---
+++ foot-ball-app/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../App.css"
+
+const NavBar = () => {
+    return (
+    <div className='navBar'>
+        <h1>FootBall App ⚽</h1>
+    </div>
+    )
+}
+
+export default NavBar
