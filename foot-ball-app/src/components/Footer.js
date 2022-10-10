b45f38113b9b56e507706cbaa996d860ba7c5f82
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-10-10 13:34:33 +0300

    create footer component

---
+++ foot-ball-app/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../App.css"
+
+const Footer = () => {
+    return (
+    <div className='footer'>
+        <p>Football &copy;2022</p>
+    </div>
+    )
+}
+
+export default Footer
