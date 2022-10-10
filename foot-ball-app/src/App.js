197c907eeb9e7ccfe1eefb02692385706269aa17
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-10-10 13:32:27 +0300

    add styling to the app

---
+++ foot-ball-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+import Content from './components/Content';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Content/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
